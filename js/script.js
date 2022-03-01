3b830dce5c3a9e29449dbd62ec8f41b2bbaf60f8
patriciabrsk <patriciabresku@gmail.com>
2022-03-01 17:38:19 +0100

    still working on it

--- js/script.js
+++ js/script.js
@@ -21,3 +21,25 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+
+const mainImage = document.getElementById('image-box');
+const titleHTML = document.getElementById('title');
+const lorem = document.getElementById('lorem');
+
+// let carouselContent = '';
+
+let currentIndex = 0;
+
+for (let i = 0; i < items.length; i++) {
+    // const carouselImage = document.createElement("div");
+    // carouselImage.classList.add('.carousel-image', ) + i;
+    carouselContent += `
+    <div class="img-wrapper">
+        <img src="${items[i]}" class="img-fluid">
+    </div>`;
+}
+
+const varticalCarousel = document.getElementById('varticalCarousel');
+varticalCarousel.innerHTML = carouselContent;
+
+
